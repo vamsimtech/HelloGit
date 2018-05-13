1e4fb726c7bd06673ddad7e19455134b64c860ea
vamsi1 <vamsi.m569@gmail.com>
2018-05-13 12:41:55 +0530

    first commit from eclipse two

--- helloGitSample/src/samplePackGit/HelloGit.java
+++ helloGitSample/src/samplePackGit/HelloGit.java
@@ -3,7 +3,7 @@ package samplePackGit;
 public class HelloGit {
 	public static void main(String[] args){
 		// greetings
-		System.out.println("Hello Git");
+		System.out.println("Hello Git two");
 	}
 
 }
